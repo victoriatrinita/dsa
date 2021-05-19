b9ca487a86d920232b7709b93194051fcd0b7548
victoriatrinita <victoriapardede@gmail.com>
2021-05-20 05:43:24 +0700

    [hackerrank] add javascript solution for 'solve me first'

---
+++ Hackerrank/Problem Solving/Warm Up/solve-me-first.js
@@ -0,0 +1,10 @@
+/*
+Title: Solve Me First
+Difficulty: Easy
+Max Score: 1
+Link: https://www.hackerrank.com/challenges/solve-me-first/problem
+*/
+
+function solveMeFirst(a, b) {
+  return a + b;
+}
