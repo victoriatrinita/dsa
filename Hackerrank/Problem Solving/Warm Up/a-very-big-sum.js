97f718a9f50bdd2938a5af9ce362de7157e4b924
victoriatrinita <victoriapardede@gmail.com>
2021-05-20 05:49:53 +0700

    [hackerrank] add javascript solution for 'a very big sum'

---
+++ Hackerrank/Problem Solving/Warm Up/a-very-big-sum.js
@@ -0,0 +1,13 @@
+/*
+Title: A Very Big Sum
+Difficulty: Easy
+Max Score: 10
+Link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+*/
+
+function aVeryBigSum(ar) {
+  for (let i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+  return sum;
+}
